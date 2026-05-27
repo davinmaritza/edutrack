7b3e9252a4c5639f2cd1e42a6cd9acfa50e6768f
Davin Maritza <rezkyandrian44@gmail.com>
2026-05-27 17:47:36 +0700

    fix(vercel): revert prisma client output to default location to solve deployment timeout

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "./prisma-client"
+import { PrismaClient } from "@prisma/client"
 // Forced reload to pick up new schema fields (notes, attendances, canEditMaterials)
 
 const prismaClientSingleton = () => {
